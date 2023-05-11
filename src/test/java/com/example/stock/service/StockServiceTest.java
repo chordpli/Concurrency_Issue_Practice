72491b04523d4cbc2899fcdb785fe6d360030b66
chordpli <chordplaylist@gmail.com>
2023-05-11 19:23:59 +0900

    feat: 문제점

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -43,5 +43,25 @@ class StockServiceTest {
 		assertEquals(99, stock.getQuantity());
 	}
 
+	@Test
+	public void 동시에_100개의_요청() throws InterruptedException {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
 
+		latch.await();
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		// 100 - ( 1* 100) = 0
+		assertEquals(0L, stock.getQuantity());
+	}
 }
