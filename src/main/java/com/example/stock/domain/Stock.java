292431c7f0ca4aa9a1cb7fe55b792ae48bf8ca9a
chordpli <chordplaylist@gmail.com>
2023-05-12 10:30:14 +0900

    feat: Optimistic Lock 활용
    장점
        - 별도의 Lock을 잡지 않으므로 PessimisticLock보다 성능상 이점
    단점
        - 업데이트가 실패했을 때 재시도 로직을 개발자가 직접 작성.
        - 충돌이 빈번하게 일어나거나 일어날 것이라고 예상된다면 PessimisticLock이 나을 수 있음.

--- src/main/java/com/example/stock/domain/Stock.java
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 @Entity
 public class Stock {
@@ -16,6 +17,9 @@ public class Stock {
 	private Long productId;
 
 	private Long quantity;
+
+	@Version
+	private Long version;
 
 	public Stock() {
 	}
